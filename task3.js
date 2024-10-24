d8dd9bffc80f7087b1a0ff90990bdd954d90aab9
vivek patidar <70385245+06vivek@users.noreply.github.com>
2024-10-24 13:55:59 +0530

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,21 @@
+//In the variables declare above assign the variables as appropriate default value instead of null.
+
+let employeeDetails={
+     Name:"vivek Patidar",
+     Email:"vivek@gmail.com",
+     Age:24,
+     address:"Indore",
+     Phone_Number:8269433839,
+     Gender:"Male",
+      married:"No",
+    }
+    console.log("Name: "+employeeDetails.Name);
+    console.log("Email: "+employeeDetails.Email);
+    console.log("Age: "+employeeDetails.Age);
+    console.log("address: "+employeeDetails.address);
+    console.log("Phone_Number: "+employeeDetails.Phone_Number);
+    console.log("Gender: "+employeeDetails.Gender);
+    console.log("married: "+employeeDetails.married);
+
+
+    
